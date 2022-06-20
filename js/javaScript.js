e9d2be3dde7e2f8ae8c7fc4b6554a2f7a84f5a05
wildo de jesus cardozo ledezma <wildolycee@gmail.com>
2022-06-20 18:31:38 +0200

    test8

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,26 +1,57 @@
 const burGer = document.querySelector(".nav-burger");
 const navList = document.querySelector(".nav-list");
-const changeIcon1 = document.querySelector("fa-chevron-up")
-const changeIcon2 = document.querySelector("fa fa-bars")
+const mobileIcon = document.querySelector("mobile-icon")
+
 
 burGer.addEventListener("click", function (event)  {
     this.classList.toggle('active')
     navList.classList.toggle("active")
-    if (this.classList.containte("active")){
-        changeIcon1.classList.replace("fa fa-bars, fa-chevron-up")
-        
-    }
-    else{
-        changeIcon2.classList.replace('fa-chevron-up, fa fa-bars')
+    if(mobileIcon.classList.contains()){}
 
-    }
+    
+    
 
 
 
 })
+const text1 = document.querySelector(".text1")
+const modalContainer1 = document.querySelector(".modal-container1");
+function submitOnClick1() {
+    modalContainer1.classList.toggle("active")
+    text1.classList.toggle("active")
 
-let changeIcon = function(icon){
-    icon.classList.toggle('fa-chevron-up')
-}
+    
+
+  }
+  const text2 = document.querySelector(".text2")
+  const modalContainer2 = document.querySelector(".modal-container2");
+  function submitOnClick2() {  
+    modalContainer2.classList.toggle("active")
+    text2.classList.toggle("active")
+
+    
+
+  }
+  const text3 = document.querySelector(".text3")
+  const modalContainer3 = document.querySelector(".modal-container3");
+  function submitOnClick3() {
+    modalContainer3.classList.toggle("active")
+    text3.classList.toggle("active")
+
+  }
+  const text4 = document.querySelector(".text4")
+  const modalContainer4 = document.querySelector(".modal-container4");
+  function submitOnClick4() {
+    modalContainer4.classList.toggle("active")
+    text4.classList.toggle("active")
+
+  }
+
+
+
+
+
+
+
 
 
